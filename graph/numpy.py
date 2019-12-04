b1b235739b772e312dda111f675ab2544a5e14a3
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-12-04 00:38:51 -0500

    tensor graph

---
+++ graph/numpy.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+np.random.seed(0)
+
+# Tensor dims
+dim = (4,5)
+
+# Initialize 3 Tensors
+t1 = np.random.rand(dim[0], dim[1])
+t2 = np.random.rand(dim[0], dim[1])
+t3 = np.random.rand(dim[0], dim[1])
+
+# Computational Graph
+a = t1 + t2
+b = a * t3
+c = np.sum(b)
+
+print('res: ', c)
+
+gradc = 1.0
+gradb = gradc * np.ones(dim)
+grada = gradb * t3
+
+gradt3 = gradb * a
+gradt2 = grada.copy()
+gradt1 = grada.copy()
+
+print('grad 1\n', gradt1)
+print('grad 2\n', gradt2)
+print('grad 3\n', gradt3)
